a1a31672467a8150733758b7f9a4d396c498580f
zhangyf <zhangyongfeng.cn@gmail.com>
2012-11-14 17:11:23 +0800

    md-assume.sh: 需要考虑掉盘（实际盘数少）和有热备盘（实际盘数多）的情况

--- us/md-auto-resume/md-assume.sh
+++ us/md-auto-resume/md-assume.sh
@@ -4,27 +4,51 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/sbin:/usr/sbin
 
 mdadm -Ss
 
-disks=0
 disk_list=''
+
+assemble() # arg1: md_num, arg2: disk_list
+{
+	if [ -z "$1" -o -z "$2" ]; then
+		return
+	fi
+
+	local mddev=/dev/md$1
+	local disks=$2
+	local disk
+	local slaves
+	local disk_name
+	mdadm -Af $mddev $disks
+	slaves=`ls /sys/block/md1/slaves 2>/dev/null`
+	if [ -z "$slaves" ]; then
+		return
+	fi
+
+	for disk in $disks
+	do
+		disk_name=${disk##*/}
+		if ! echo $slaves | grep -w "$disk_name" 1>/dev/null 2>&1; then
+			mdadm -a $mddev $disk
+		fi
+	done
+}
 
 while read LINE
 do
         echo "$LINE"
         if `echo "$LINE" | grep "Get md" >/dev/null`; then
-                disks=${LINE##*disks=}
-                disks=${disks%%,*}
+		if [ ! -z "$disk_list" ]; then
+			assemble $md_num "$disk_list"
+			disk_list=''
+		fi
                 md_num=${LINE##*md_num: }
                 continue
         fi
-        if [ $disks -ge 0 ]; then
-                disk_list="$disk_list $LINE"
-                let "disks=disks-1"
-        fi
-        if [ $disks -eq 0 ] && [ ! -z "$disk_list" ]; then
-                2>/dev/null mdadm -Af /dev/md$md_num $disk_list
-                disks=0
-                disk_list=''
-        fi
+        disk_list="$disk_list $LINE"
 done<<EOF
 $(/usr/local/bin/mdinfo 2>/dev/null)
 EOF
+
+if [ ! -z "$disk_list" ]; then
+	assemble $md_num "$disk_list"
+fi
+
