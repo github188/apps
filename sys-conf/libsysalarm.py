f5998f755b153f6f16319cd381329307a9afa4b5
GongLei <gongl@jwele.com.cn>
2012-11-28 17:46:54 +0800

    增加设置告警方式接口

--- sys-conf/libsysalarm.py
+++ sys-conf/libsysalarm.py
@@ -20,3 +20,15 @@ def alarm_email_set(email=email_conf()):
 
 def alarm_email_test():
 	return True,'电子邮件配置测试成功'
+
+_alarm_module = ['power', 'disk', 'vg', 'temperature', 'fan']
+_alarm_category = ['buzzer', 'sys-led', 'email']
+
+def get_alarm_module():
+	return str(_alarm_module)
+
+def get_alarm_category():
+	return str(_alarm_category)
+
+def alarm_set(module, switch, category):
+	return True, ''
