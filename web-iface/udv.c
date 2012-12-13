00be186e728ed7042906f52a6c0de86abd3ee127
GongLei <gongl@jwele.com.cn>
2012-12-13 18:21:21 +0800

    udv输出vg名称

--- web-iface/udv.c
+++ web-iface/udv.c
@@ -130,11 +130,11 @@ void list_udv(list_type_t t)
 
 			if (printed==0)
 				printf("\n\t\t{\"name\":\"%s\", \"capacity\":%llu, \"state\":\"%s\", \"vg\":\"%s\", \"combin\":\"%s|%llu\"}",
-						udv->name, (unsigned long long)udv->geom.capacity, udv_state, udv->vg_dev,
+						udv->name, (unsigned long long)udv->geom.capacity, udv_state, udv->vg_name,
 						udv->name, (unsigned long long)udv->geom.capacity);
 			else
 				printf(",\n\t\t{\"name\":\"%s\", \"capacity\":%llu, \"state\":\"%s\", \"vg\":\"%s\", \"combin\":\"%s|%llu\"}",
-						udv->name, (unsigned long long)udv->geom.capacity, udv_state, udv->vg_dev,
+						udv->name, (unsigned long long)udv->geom.capacity, udv_state, udv->vg_name,
 						udv->name, (unsigned long long)udv->geom.capacity);
 			printed++;
 		}
