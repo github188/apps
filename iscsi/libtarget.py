a5f1068a8c43ffcf1ba4bed442177ee0258ae7d9
GongLei <gongl@jwele.com.cn>
2013-01-14 11:26:52 +0800

    ..

--- iscsi/libtarget.py
+++ iscsi/libtarget.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
+import commands
 #os.chdir(os.path.dirname(__file__))
 
 from libiscsicommon import *
 from uuid import uuid1
+from libmd import md_info_mddevs
 
 JW_ISCSI = '/opt/jw-conf/iscsi'
 JW_ISCSI_DFT_TARGET = 'default-target'
@@ -170,6 +173,80 @@ def iSCSISetTargetAttr(tgt_name, attr, value):
 				return True, 'Target属性设置成功!写配置文件失败! %s' % msg
 			return (True, 'Target属性设置成功！')
 	return (False, 'Target属性设置失败！')
+
+# 将SCST配置文件中的md节点名称替换为UUID
+def iSCSI_SCST_cfg_convert():
+	try:
+		f = open(SCST.CFG)
+		xx = f.read()
+		f.close()
+
+		mddev_list = re.findall('\/dev\/md\d+', xx)
+		if len(mddev_list) == 0:
+			return
+		for mdinfo in md_info_mddevs()['rows']:
+			if mdinfo['dev'] in mddev_list:
+				xx = xx.replace(mdinfo['dev'], mdinfo['raid_uuid'])
+		f = open(SCST.CFG, 'w')
+		f.write(xx)
+		f.close()
+
+		os.rename(tmp_file, SCST.CFG)
+	except:
+		pass
+	return
+
+# 将SCST配置文件中的UUID名称转换为节点名称
+# 并且检查对应的分区是否存在，如果不存在，则记录日志删除对应的配置
+def iSCSI_SCST_cfg_restore():
+	tmp_file = '/tmp/.iscsi-scst-conf'
+	try:
+		f = open(SCST.CFG)
+		xx = f.read()
+		f.close()
+
+		uuid_list = re.findall('\w{8}:\w{8}:\w{8}:\w{8}', xx)
+		if len(uuid_list) == 0:
+			return
+		for mdinfo in md_info_mddevs()['rows']:
+			if mdinfo['raid_uuid'] in uuid_list:
+				xx = xx.replace(mdinfo['raid_uuid'], mdinfo['dev'])
+
+		f = open(tmp_file, 'w')
+		f.write(xx)
+		f.close()
+	except:
+		pass
+	return tmp_file
+
+def iSCSIUpdateCFG():
+	try:
+		_cmd = 'scstadmin -write_config %s' % SCST.CFG
+		ret,msg = commands.getstatusoutput(_cmd)
+		if ret == 0:
+			iSCSI_SCST_cfg_convert()
+			return True, '写入配置文件成功'
+		else:
+			return False, msg
+	except:
+		return False, '写入配置文件失败，未知原因'
+
+def iSCSIRestoreCFG():
+	try:
+		_cmd = 'scstadmin -config %s' % iSCSI_SCST_cfg_restore()
+		ret,msg = commands.getstatusoutput(_cmd)
+		if ret == 0:
+			return True, '加载iSCSI配置成功'
+		# 设置默认配置，保证target能启动
+		iSCSISetDefaultTarget(force = True)
+		_cmd = 'scstadmin -config %s' % SCST.CFG
+		ret,msg = commands.getstatusoutput(_cmd)
+		if ret == 0:
+			return True, '加载用户iSCSI配置失败，尝试加载默认配置成功'
+		else:
+			return False, '加载用户iSCSI配置失败，尝试加默认配置失败'
+	except:
+		return False, '加载iSCSI配置失败，未知原因'
 
 # 测试代码
 if __name__ == '__main__':
