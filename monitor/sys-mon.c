43f45aa9e495580fd16e7fa36313171c7112257f
GongLei <gonglei@jwele.com.cn>
2013-02-01 07:02:43 +0800

    增加前面板灯设置功能；加载self_run配置有问题，暂时注释掉代码

--- monitor/sys-mon.c
+++ monitor/sys-mon.c
@@ -111,7 +111,7 @@ int main()
 	signal(SIGPIPE, SIG_IGN);
 	signal(SIGTERM, mon_release);
 	signal(SIGINT, mon_release);
-	signal(SIGALRM, do_interval_check);
+	//signal(SIGALRM, do_interval_check);
 
 	log_init();
 	sys_mon_load_conf();
@@ -122,7 +122,7 @@ int main()
 	dump_sys_global();
 #endif
 
-	alarm(CHECK_INTVAL);
+	//alarm(CHECK_INTVAL);
 	mon_io.sockfd = mon_serv_create();
 
 	// ev loop
