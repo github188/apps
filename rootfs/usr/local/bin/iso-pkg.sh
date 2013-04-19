7c7f694c26bdea1dd85b8ebc2a9da5e861371db7
zhangyf <zhangyf@jwele.com.cn>
2013-04-19 10:41:06 +0800

    修改启动脚本和打包脚本

--- rootfs/usr/local/bin/iso-pkg.sh
+++ rootfs/usr/local/bin/iso-pkg.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+PKG_STORE_DIR=/home
 JW_CONF_DIR=/opt/jw-conf
+
+conf_backup()
+{
+	rm -rf conf_bak
+	mkdir conf_bak
+	cp /opt/* conf_bak/ -a
+}
+
+conf_restore()
+{
+	cp conf_bak/* /opt/ -a
+	rm -rf conf_bak
+}
 
 make_default_conf()
 {
@@ -11,6 +25,9 @@ make_default_conf()
 	mkdir -pv $JW_CONF_DIR/{disk,iscsi,nas,system}
 	cat /dev/null > "$JW_CONF_DIR"/nas/self-load.sh
 	chmod +x "$JW_CONF_DIR"/nas/self-load.sh
+
+	# net
+	network --default
 
 	# clear log
 	find /var/log/ -type f -exec rm -f {} \;
@@ -23,6 +40,8 @@ pkg_root_preprocess()
 	# be sure /etc/sudoers has right privilege
 	chown root:root /etc/sudoers
 	chmod 440 /etc/sudoers
+	
+	rm -f /etc/udev/rules.d/70-persistent-net.rules
 }
 
 pkg_root()
@@ -31,30 +50,30 @@ pkg_root()
 
 	# check if mounted /
 	echo "Checking rootfs mounted..."
-	mount | grep \/media\/sda1 > /dev/null
+	mount | grep $PKG_STORE_DIR\/sda1 > /dev/null
 	if [ $? -ne 0 ]; then
 		echo "rootfs not mount! mounting now ...."
-		mkdir -p /media/sda1
-		mount /dev/sda1 /media/sda1
+		mkdir -p $PKG_STORE_DIR/sda1
+		mount /dev/sda1 $PKG_STORE_DIR/sda1
 	fi
 
 	# check again
-	mount | grep \/media\/sda1 > /dev/null
+	mount | grep $PKG_STORE_DIR\/sda1 > /dev/null
 	[ $? -ne 0 ] && echo "rootfs not mounted!" && exit -1
 	
 	echo "mount rootfs OK! package start ..."
-	rm -f /media/root.tgz
-	cd /media/sda1
-	tar zcf ../root.tgz ./*
-	cd -
+	cd $PKG_STORE_DIR/sda1
+	tar zcf $PKG_STORE_DIR/root.tgz ./
+	cd $PKG_STORE_DIR
+	umount sda1 && rm -rf sda1
 	echo "root.tgz packaged OK!"
 }
 
 pkg_opt()
 {
 	echo "opt.tgz package start ..."
-	cd /opt/
-	tar zcf /media/opt.tgz ./*
+	cd /opt
+	tar zcf $PKG_STORE_DIR/opt.tgz ./
 	cd -
 	echo "opt.tgz packaged OK!"
 }
@@ -62,14 +81,17 @@ pkg_opt()
 pkg_local()
 {
 	echo "local.tgz package start ..."
-	cd /usr/local/
-	tar zcf /media/local.tgz ./*
+	cd /usr/local
+	tar zcf $PKG_STORE_DIR/local.tgz ./
 	cd -
 	echo "local.tgz packaged OK!"
 }
 
+cd $PKG_STORE_DIR
+rm -rf root.tgz local.tgz opt.tgz
+conf_backup
 make_default_conf
 pkg_root
 pkg_local
 pkg_opt
-
+conf_restore
