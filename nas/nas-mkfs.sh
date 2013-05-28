eb7772833de6cb07ffd8b79c678d851b22f0719a
zhangyf <zhangyf@jwele.com.cn>
2013-05-28 15:17:42 +0800

    nas/nas-mkfs.sh 格式化xfs时需要-f选项

--- nas/nas-mkfs.sh
+++ nas/nas-mkfs.sh
@@ -6,5 +6,9 @@ filesystem="$2"
 # set default filesystem to ext4
 [ -z "$filesystem" ] && filesystem="ext4"
 
-2>/dev/null mkfs."$filesystem" $dev | tr-simple
+if [ "$filesystem" = "xfs" ]; then
+	option="-f"
+fi
+
+2>/dev/null mkfs."$filesystem" $option $dev | tr-simple
 
