40fc3d9bbdd942eddcf071e2ebf8334d328561ad
puter <wuyb@jwele.com.cn>
2013-06-17 21:56:47 +0800

    修正修改主机名时，不修改hosts文件

--- sys-conf/libsysconfig.py
+++ sys-conf/libsysconfig.py
@@ -50,9 +50,12 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #
 
 """
-HOSTS_Content = """127.0.0.1	localhost
+HOSTS_Content = """127.0.0.1  JW-Linux.localdomain    JW-Linux
 
-::1     localhost ip6-localhost ip6-loopback
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
 ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
 
@@ -119,8 +122,8 @@ def hosts(value):
 			fileList = hosts_conf.readlines()
 			hosts_file = ''
 			for fileLine in fileList:
-				if len(fileLine.split('127.0.0.1	')) > 1:
-					hosts_file = hosts_file + '127.0.0.1	'+value.hosts_set+'\n'
+				if len(fileLine.split('127.0.0.1')) > 1:
+					hosts_file = hosts_file + '127.0.0.1    '+value.hosts_set+'.localdomain    '+value.hosts_set+'\n'
 				else:
 					hosts_file = hosts_file + fileLine
 		finally:
