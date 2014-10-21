f3db1986587c2058b9202b7258db6ddb5d693a20
liyunteng <liyt@jwele.com.cn>
2014-10-21 11:19:15 +0800

    add sas_sysled.c

---
+++ led/cmd/sas_sysled.c
@@ -0,0 +1,39 @@
+/*
+ *更新时间: < 修改人[liyunteng] 2014/10/21 11:15:13 >
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <error.h>
+#include <string.h>
+
+#include "../daemon/sysled.h"
+
+#define ARGIVALID -1
+
+
+void print_help(void)
+{
+	fprintf(stderr, "sas_sysled <on|off>\n");
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 1) {
+		print_help();
+		return ARGIVALID;
+	}
+	if (!strcmp(argv[1],"on")) {
+		return sb_gpio28_set(true);
+	}
+
+	if (!strcmp(argv[1], "off")) {
+		return sb_gpio28_set(false);
+	}
+
+	return ARGIVALID;
+}
+
+
+
+
+
